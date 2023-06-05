1c16b7ea91f5c365f9e05b7c876e334a23a93f33
Yahya Abad <yabad@c2r9p5.1337.ma>
2023-06-05 15:18:10 +0100

    add includes dir

---
+++ includes/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabad <yabad@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 15:02:21 by yabad             #+#    #+#             */
+/*   Updated: 2023/06/05 15:04:14 by yabad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <signal.h>
+# include "../Libft/libft.h"
+
+#endif
