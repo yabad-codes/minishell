20a19b11e14a5d0830451868beee23c59a75c9cc
Yahya ABAD <yahya.abad9@gmail.com>
2023-06-05 16:20:25 +0100

    add lexer

---
+++ lexer/lexer.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabad <yabad@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 15:51:29 by yabad             #+#    #+#             */
+/*   Updated: 2023/06/05 16:19:47 by yabad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/lexer.h"
+
+t_node	*get_tokens(char *input)
+{
+	return (NULL);
+}
+
+void	lexer(char *input)
+{
+	t_node	*tokens;
+
+	tokens = get_tokens(input);
+	return ;
+}
