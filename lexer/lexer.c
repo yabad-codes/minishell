d507c0927f186e810b61d08b579b4892fc009078
Yahya ABAD <yahya.abad9@gmail.com>
2023-06-05 16:46:16 +0100

    add lexer and get_tokens functions prototype

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,20 +6,21 @@
 /*   By: yabad <yabad@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 15:51:29 by yabad             #+#    #+#             */
-/*   Updated: 2023/06/05 16:19:47 by yabad            ###   ########.fr       */
+/*   Updated: 2023/06/05 16:44:46 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lexer.h"
 
-t_node	*get_tokens(char *input)
+t_list	*get_tokens(char *input)
 {
+	(void)input;
 	return (NULL);
 }
 
 void	lexer(char *input)
 {
-	t_node	*tokens;
+	t_list	*tokens;
 
 	tokens = get_tokens(input);
 	return ;
