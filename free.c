11418293094a8b79f45a3e338d4f1ec80c6c50c5
ael-maar <anouarelmaaroufi@gmail.com>
2023-06-28 19:54:44 +0100

    Adding a free.c file
    Adding a free.c file that includes a function free_tokens_and_exit() that used when an error occurs in the parsing phase

--- free.c
+++ free.c
@@ -1,31 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/05 15:02:21 by yabad             #+#    #+#             */
-/*   Updated: 2023/06/28 19:10:57 by ael-maar         ###   ########.fr       */
+/*   Created: 2023/06/28 19:08:42 by ael-maar          #+#    #+#             */
+/*   Updated: 2023/06/28 19:15:28 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
+#include "parser.h"
 
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <signal.h>
-# include "lexer.h"
-# include "../Libft/libft.h"
-
-# define ANSI_COLOR_SKY_BLUE   "\033[1;36m"
-# define ANSI_COLOR_RESET      "\x1b[0m"
-
-t_token	*lexer(char *input);
-
-void    free_tokens_and_exit(t_token *token_head);
-
-#endif
+void    free_tokens_and_exit(t_token *token_head)
+{
+    // implementation of the this function
+}
